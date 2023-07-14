2df042070337f1b395a01a0d11ca7a006ba46915
WNicoleS <133277288+WNicoleS@users.noreply.github.com>
2023-07-13 20:59:48 -0500

    Added add function

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,21 @@
 
+var itemForm = $("#item-form");
+var itemList = $("#item-list");
+var itemInput = $("#item-input");
+
+itemForm.on("submit", function(event) {
+  event.preventDefault();
+  
+  var itemText = itemInput.val();
+
+  if (itemText !== "") {
+    var listItem = $("<li class='ui-state-default  ui-sortable-handle'>");
+    listItem.text(itemText);
+    itemList.append(listItem);
+    itemInput.val("");
+  }
+})
+
 $(function () {
     $('#calender-list').sortable();
   });
