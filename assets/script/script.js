672afbdded53e31f35c64b9d299e51419a7fa123
Faysia Lopez <faysia.lopez@frontiersin.net>
2023-07-17 18:54:48 -0500

    added local storage to javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,13 @@
-
 var itemForm = $("#item-form");
 var itemList = $("#item-list");
 var itemInput = $("#item-input");
+var saveButton = $('#save-option');
 
-itemForm.on("submit", function(event) {
-  event.preventDefault();
-  
+var listContents = [];
+
+function renderItems() {
   var itemText = itemInput.val();
+  listContents.push(itemText);
 
   if (itemText !== "") {
     var listItem = $("<li class='ui-state-default  ui-sortable-handle'>");
@@ -14,8 +15,32 @@ itemForm.on("submit", function(event) {
     itemList.append(listItem);
     itemInput.val("");
   }
+}
+
+itemForm.on("submit", function (event) {
+  event.preventDefault();
+
+  renderItems();
 })
 
+function init()  {
+  var storedItems = JSON.parse(localStorage.getItem("items"));
+
+  if (storedItems !== null) {
+    listContents = storedItems;
+  }
+
+  renderItems();
+}
+
+saveButton.on("click", function (event) {
+  event.preventDefault();
+
+  localStorage.setItem("items", JSON.stringify(listContents));
+
+});
+
 $(function () {
-    $('#item-list').sortable();
-  });
+  $('#item-list').sortable();
+});
+
