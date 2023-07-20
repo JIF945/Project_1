0f286647cb2c70f8513cb97677d68b3dddef87fc
Faysia Lopez <faysia.lopez@frontiersin.net>
2023-07-19 21:22:42 -0500

    added html for saved lists

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,57 +7,60 @@ var listContents = [];
 
 //renders list items to the page
 function renderItems() {
-  var itemText = itemInput.val();
-  listContents.push(itemText);
+    var itemText = itemInput.val();
+    listContents.push(itemText);
 
-  if (itemText !== "") {
-    var listItem = $("<li class='ui-state-default  ui-sortable-handle'>");
-    listItem.text(itemText);
-    itemList.append(listItem);
-    itemInput.val("");
-  }
+    if (itemText !== "") {
+        var listItem = $("<li class='ui-state-default  ui-sortable-handle'>");
+        listItem.text(itemText);
+        itemList.append(listItem);
+        itemInput.val("");
+    }
 }
 
 function storeItems() {
-  localStorage.setItem("items", JSON.stringify(listContents));
+    localStorage.setItem("items", JSON.stringify(listContents));
 }
 
 //event listener for the "add" button
 itemForm.on("submit", function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  renderItems();
+    renderItems();
 })
 
-function init()  {
-  var storedItems = JSON.parse(localStorage.getItem("items"));
+function init() {
+    var storedItems = JSON.parse(localStorage.getItem("items"));
 
-  if (storedItems !== null) {
-    listContents = storedItems;
-  }
+    if (storedItems !== null) {
+        listContents = storedItems;
+    }
 
-  renderItems();
+    renderItems();
 }
 
 //event listener for the "save" button
 saveButton.on("click", function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  storeItems();
-  saveList();
-  
+    storeItems();
+
+    saveList();
+
+
 });
 
-function saveList()  {
-  var savedListEl = document.createElement('a');
-  savedListEl.textContent = "List #1";
-  document.body.appendChild(savedListEl);
-  savedListEl.href = ('lists.html');
-  savedListEl.target = '_blank';
+function saveList() {
+    var savedListEl = document.createElement('a');
+    var savedListArea = document.getElementById("saved-list-links");
+    savedListEl.textContent = "Saved List";
+    savedListArea.appendChild(savedListEl);
+    savedListEl.href = ('lists.html');
+    savedListEl.target = '_blank';
 }
 
 $(function () {
-  $('#item-list').sortable();
+    $('#item-list').sortable();
 });
 
 
@@ -72,38 +75,38 @@ var navLoc = document.querySelector("#nav-loc");
 var navAbout = document.querySelector("#nav-about");
 var navSaved = document.querySelector("#nav-saved-list")
 
-navList.addEventListener("click", function() {
-  if (navList !== false) {
-    list.setAttribute("style", "display:block");
-    locationOfProduct.setAttribute("style", "display:none");
-    about.setAttribute("style", "display:none");
-    savedList.setAttribute("style", "display:none");
-  }
+navList.addEventListener("click", function () {
+    if (navList !== false) {
+        list.setAttribute("style", "display:block");
+        locationOfProduct.setAttribute("style", "display:none");
+        about.setAttribute("style", "display:none");
+        savedList.setAttribute("style", "display:none");
+    }
 })
 
-navLoc.addEventListener("click", function() {
-  if (navLoc !== false) {
-    list.setAttribute("style", "display:none");
-    locationOfProduct.setAttribute("style", "display:block");
-    about.setAttribute("style", "display:none");
-    savedList.setAttribute("style", "display:none");
-  }
+navLoc.addEventListener("click", function () {
+    if (navLoc !== false) {
+        list.setAttribute("style", "display:none");
+        locationOfProduct.setAttribute("style", "display:block");
+        about.setAttribute("style", "display:none");
+        savedList.setAttribute("style", "display:none");
+    }
 })
 
-navAbout.addEventListener("click", function() {
-  if (navAbout !== false) {
-    list.setAttribute("style", "display:none");
-    locationOfProduct.setAttribute("style", "display:none");
-    about.setAttribute("style", "display:block");
-    savedList.setAttribute("style", "display:none");
-  }
-})
-
-navSaved.addEventListener("click", function() {
+navAbout.addEventListener("click", function () {
     if (navAbout !== false) {
-      list.setAttribute("style", "display:none");
-      locationOfProduct.setAttribute("style", "display:none");
-      about.setAttribute("style", "display:none");
-      savedList.setAttribute("style", "display:block");
+        list.setAttribute("style", "display:none");
+        locationOfProduct.setAttribute("style", "display:none");
+        about.setAttribute("style", "display:block");
+        savedList.setAttribute("style", "display:none");
+    }
+})
+
+navSaved.addEventListener("click", function () {
+    if (navAbout !== false) {
+        list.setAttribute("style", "display:none");
+        locationOfProduct.setAttribute("style", "display:none");
+        about.setAttribute("style", "display:none");
+        savedList.setAttribute("style", "display:block");
     }
 })
