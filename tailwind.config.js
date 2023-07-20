5090b2cbfc356fd1c959adba1fdbc0b78e73688b
SKhai77 <orionkhai@gmail.com>
2023-07-19 19:07:53 -0500

    Updated the html page, css, banner, and color

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'firstcolor': '#EAE7DC',
+        'secondcolor': '#D8C3A5',
+        'thirdcolor': '#8E8D8A',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+    },
+  }
